9ef1307e94eb620e2e7a279d9a01e96e145b6e96
Mariko Cristiana Iorga <mariko.cristiana@dextra-sw.com>
2022-10-16 00:06:06 -0300

    added functional search bar barebones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// pages
 import { HomeComponent } from './pages/home/home.component';
+
+// components
 import { PokeListComponent } from './pages/home/components/poke-list/poke-list.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { SearchBarComponent } from './shared/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +18,7 @@ import { HeaderComponent } from './shared/header/header.component';
     HomeComponent,
     PokeListComponent,
     HeaderComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/components/poke-list/poke-list.component.ts
+++ src/app/pages/home/components/poke-list/poke-list.component.ts
@@ -11,15 +11,27 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 export class PokeListComponent implements OnInit {
 
   public pokelist: Array<any> = [];
-  public error: boolean = false;
+  public pokedex:  Array<any> = [];
+  public error:    boolean    = false;
 
   constructor(private pokeApiService: PokeApiService) { }
 
   ngOnInit(): void {
     this.pokeApiService.getPokedex().subscribe({
-      next:  (res) => this.pokelist = res.results,
-      error: (err) => this.error = true
+      next:  (res) => {
+        this.pokedex  = res.results;
+        this.pokelist = res.results;
+      },
+      error: (err) => {
+        this.error = true;
+      }
     })
+  }
+
+  public findPokemon(event: string) {
+    this.pokelist = this.pokedex.filter((pokemon: any) => {
+      return pokemon.name.startsWith(event.toLowerCase());
+    });
   }
 
 }
